09611e64507a026299a67b1d49375bebdc376989
hachi8833 <hachi8833@gmail.com>
2018-12-27 10:38:16 +0900

    Refactor codes

--- main.go
+++ main.go
@@ -1,66 +1,71 @@
 package main
 
 import (
-	"encoding/csv"
-	"fmt"
-	"github.com/logrusorgru/aurora"
-	"io"
-	"os"
+  "encoding/csv"
+  "fmt"
+  "github.com/logrusorgru/aurora"
+  "io"
+  "os"
 )
+
+// DELIMITER holds a string to delimition.
+const DELIMITER = " "
 
 func main() {
 
-	fp := readStdin()
+  fp := readStdin()
 
-	reader := csv.NewReader(fp)
-	reader.Comma = ','
-	reader.LazyQuotes = true
+  reader := csv.NewReader(fp)
+  reader.Comma = ','
+  reader.LazyQuotes = true
 
-	for {
-		record, err := reader.Read()
-		if err == io.EOF {
-			break
-		} else if err != nil {
-			panic(err)
-		}
-		for i, col := range record {
-			rotateColor(i, col)
-		}
-		fmt.Println()
-	}
+  for {
+    record, err := reader.Read()
+    if err == io.EOF {
+      break
+    } else if err != nil {
+      panic(err)
+    }
+    for i, col := range record {
+      rotateColor(i, col)
+    }
+    fmt.Println()
+  }
 }
 
-func rotateColor(c int, col interface{}) {
-	switch c % 7 {
-	case 0:
-		fmt.Print(aurora.Magenta(col), " ")
-	case 1:
-		fmt.Print(aurora.Blue(col), " ")
-	case 2:
-		fmt.Print(aurora.Brown(col), " ")
-	case 3:
-		fmt.Print(aurora.Green(col), " ")
-	case 4:
-		fmt.Print(aurora.Gray(col), " ")
-	case 5:
-		fmt.Print(aurora.Cyan(col), " ")
-	case 6:
-		fmt.Print(aurora.Red(col), " ")
-	}
-	return
+// torateColor exports the colored columns with circuration.
+func rotateColor(i int, col interface{}) {
+  switch i % 7 {
+  case 0:
+    fmt.Print(aurora.Magenta(col), DELIMITER)
+  case 1:
+    fmt.Print(aurora.Blue(col), DELIMITER)
+  case 2:
+    fmt.Print(aurora.Brown(col), DELIMITER)
+  case 3:
+    fmt.Print(aurora.Green(col), DELIMITER)
+  case 4:
+    fmt.Print(aurora.Gray(col), DELIMITER)
+  case 5:
+    fmt.Print(aurora.Cyan(col), DELIMITER)
+  case 6:
+    fmt.Print(aurora.Red(col), DELIMITER)
+  }
+  return
 }
 
+// readStdin returns the CSV from stdin.
 func readStdin() *os.File {
-	var fp *os.File
-	if len(os.Args) < 2 {
-		fp = os.Stdin
-	} else {
-		var err error
-		fp, err = os.Open(os.Args[1])
-		if err != nil {
-			panic(err)
-		}
-		defer fp.Close()
-	}
-	return fp
+  var fp *os.File
+  if len(os.Args) < 2 {
+    fp = os.Stdin
+  } else {
+    var err error
+    fp, err = os.Open(os.Args[1])
+    if err != nil {
+      panic(err)
+    }
+    defer fp.Close()
+  }
+  return fp
 }
